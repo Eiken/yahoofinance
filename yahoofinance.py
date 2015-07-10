0a6f8b826b7bc7dff22739504742b5957f166980
Erik Johansson <erjo@3d12.fido.hq>
2015-07-10 17:12:17 +0200

    support wild search for stocks

--- yahoofinance.py
+++ yahoofinance.py
@@ -1,24 +1,93 @@
 import requests
 import urllib
+import sys
 from datetime import datetime
 from datetime import timedelta
 import re
 import json
-from willie import module
+#from willie import module
 
 #tickers = 'PRIC-B.ST'
 #tickers = 'G5EN.ST'
 #tickers = 'G5EN.ST,PRIC-B.ST'
+#tickers = 'apple,pricer'
 
 #arg = '3m'
 #arg = '1y'
 #arg = '15d'
 #arg = None
 
+def getTicker(name):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={0}&callback=YAHOO.Finance.SymbolSuggest.ssCallback".format(name)
+    response = requests.get(url)
+    #print response.content
+    html = response.content.lstrip("YAHOO.Finance.SymbolSuggest.ssCallback(").rstrip(")")
+    data = json.loads(html)
+    result = data.get('ResultSet').get('Result')
+    if result:
+        #try to find swedish stocks first
+        for r in result:
+            if r.get('exch') == 'STO':
+                return r.get('symbol')
+
+        return result[0].get('symbol')
+    else:
+        return None
+
+
+def getCurrentQuote(ticker):
+    url = 'https://query.yahooapis.com/v1/public/yql?'
+    q = {
+        'q': 'select * from yahoo.finance.quote where symbol in ("{0}")'.format(ticker),
+        'format': 'json',
+        'env': 'store://datatables.org/alltableswithkeys'
+    }
+
+    query = url + urllib.urlencode(q)
+    result = requests.get(query)
+    dic = json.loads(result.content)
+    quote = dic.get('query').get('results').get('quote')
+    if type(quote)  == list:
+        return None, None
+
+    latest = quote.get('LastTradePriceOnly')
+    if latest:
+        percentage = float(quote.get('Change'))
+    else:
+        percentage = None
+
+    return latest, percentage
+
+
+def getQuoteForRange(ticker, start, end):
+    url = 'https://query.yahooapis.com/v1/public/yql?'
+    q = {
+        'q': 'select * from yahoo.finance.historicaldata where symbol = "{0}" and startDate = "{1}" and endDate = "{2}"'.format(ticker, start, end),
+        'format': 'json',
+        'env': 'store://datatables.org/alltableswithkeys'
+    }
+    query = url + urllib.urlencode(q)
+    result = requests.get(query)
+    dic = json.loads(result.content)
+    results = dic.get('query').get('results')
+    if results:
+        quoteList = results.get('quote')
+
+        if len(quoteList) > 1:
+            latest = float(quoteList[0].get('Close'))
+            old = float(quoteList[-1].get('Close'))    
+
+            percentage = (latest - old) / old
+            percentage *= 100.0
+
+        return latest, old, percentage
+    else:
+        return None, None, None
+
 @module.commands('yftest')
 def yf(bot, trigger):  
 #def test(arg, tickers):
-    url = 'https://query.yahooapis.com/v1/public/yql?'
+
     
     tickers = trigger.group(3)
     arg = trigger.group(4)
@@ -28,7 +97,7 @@ def yf(bot, trigger):
         return
 
     tickers = tickers.split(',')
-    totalPercentage = 0.0    
+    totalPercentage = []
 
     for ticker in tickers:
 
@@ -62,56 +131,35 @@ def yf(bot, trigger):
             endDateString = endDate.strftime("%Y-%m-%d")
 
 
-            q = {
-                'q': 'select * from yahoo.finance.historicaldata where symbol = "{0}" and startDate = "{1}" and endDate = "{2}"'.format(ticker, startDateString, endDateString),
-                'format': 'json',
-                'env': 'store://datatables.org/alltableswithkeys'
-            }
-            query = url + urllib.urlencode(q)
-            result = requests.get(query)
-            dic = json.loads(result.content)
-            quoteList = dic.get('query').get('results').get('quote')
-
-            if len(quoteList) > 1:
-                latest = float(quoteList[0].get('Close'))
-                old = float(quoteList[-1].get('Close'))    
-
-                percentage = (latest - old) / old
-                percentage *= 100.0
-
-                totalPercentage += percentage
-
-
-                out = "{0} period quote: startdate: {1}; quote: {2}, enddate {3}; quote {4}. change: ({5:.2f}%)".format(ticker, startDateString, old, endDateString, latest, percentage)
-                #print out
-                bot.say(out)
+            latest, old, percentage = getQuoteForRange(ticker, startDateString, endDateString)
+            if not latest:
+                ticker = getTicker(ticker)
+                latest, old, percentage = getQuoteForRange(ticker, startDateString, endDateString)
+            if percentage:
+                totalPercentage.append(percentage)
+            else:
+                percentage = 0.0
+            out = "{0} period quote: startdate: {1}; quote: {2}, enddate {3}; quote {4}. change: ({5:.2f}%)".format(ticker, startDateString, old, endDateString, latest, percentage)
+            #print out
+            bot.say(out)
             
             
             
 
         else:
-            q = {
-                'q': 'select * from yahoo.finance.quote where symbol in ("{0}")'.format(ticker),
-                'format': 'json',
-                'env': 'store://datatables.org/alltableswithkeys'
-            }
-
-            query = url + urllib.urlencode(q)
-            result = requests.get(query)
-            dic = json.loads(result.content)
-            quote = dic.get('query').get('results').get('quote')
-            latest = quote.get('LastTradePriceOnly')
-            percentage = float(quote.get('Change'))
-            totalPercentage += percentage
+            latest, percentage = getCurrentQuote(ticker)
+            if not latest:
+                ticker = getTicker(ticker)
+                latest, percentage = getCurrentQuote(ticker)
+            if percentage:
+                totalPercentage.append(percentage)
 
             out = 'Latest {0} quote is: {1} ({2}%)'.format(ticker, latest, percentage)
             #print out
             bot.say(out)
 
-    numTickers = len(tickers)
-    if numTickers > 1:
-        #print numTickers
-        out = 'Average change: {0:.2f}%'.format(totalPercentage/float(numTickers))
+    if len(totalPercentage) > 1:
+        out = 'Average change: {0:.2f}%'.format(sum(totalPercentage)/float(len(totalPercentage)))
         #print out
         bot.say(out)
 
